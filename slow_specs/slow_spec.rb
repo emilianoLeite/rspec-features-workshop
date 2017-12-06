8cf877bb491b16646e7edf653d27ef6fe80c7305
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-06 17:28:24 -0200

    Remove unnecessary examples.

--- slow_specs/slow_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'Fuubar' do
-  7.times do
-    it 'is awesome' do
-      expected = Random.rand > 0.5 ? true : false
-      sleep 1
-      expect(true).to be expected
-    end
-  end  
-end
