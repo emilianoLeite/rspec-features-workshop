1df8182a66f9899a0b90c0979274245d477f78b4
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-08 13:57:20 -0200

    Move setup to a before block

--- spec/features/show_users_spec.rb
+++ spec/features/show_users_spec.rb
@@ -3,20 +3,23 @@ require 'rails_helper'
 describe 'As a user', js: true, type: :feature do
 
   specify 'I can view the details of other users' do
+    # List page
     user = User.create(name: 'User 1', email: 'user@email.com')
     visit '/users'
     expect(page).to have_content 'Users'
 
     find(".js-show-#{user.id}").click
 
+    # Show page
     expect(page).to have_content 'User 1'
     expect(page).to have_content 'user@email.com'
   end
 
   context 'inside the user details page' do
-    let(:user) { User.create(name: 'User 1', email: 'user@email.com') }
-
-    before { visit "/users/#{user.id}" }
+    before do
+      user = User.create(name: 'User 1', email: 'user@email.com') 
+      visit "/users/#{user.id}"
+    end
 
     specify 'I can go back to the users list' do
       click_link 'Back'
