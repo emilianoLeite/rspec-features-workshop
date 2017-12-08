e55d40b398ba0de016bb95f97c027514f5d2f5ad
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-08 14:22:47 -0200

    Add realistic stub example

--- stubs/1_spec.rb
+++ stubs/1_spec.rb
@@ -4,7 +4,7 @@ describe 'Stubs' do
   subject(:stack) { Stack.new(['last_item']) }
 
   describe "Stack#pop" do
-    before { allow(subject).to receive(:pop).and_return 'new_item' }
+    before { allow(subject).to receive(:poop).and_return 'new_item' }
     
     it 'stubs the supplied method' do
       expect(stack.pop).to eq 'new_item'
--- stubs/2_spec.rb
+++ stubs/2_spec.rb
@@ -1,19 +1,35 @@
-class Controller
-  def index
-    @products = Product.all
+class UserAuthenticator
+  def self.authenticate(user)
+    user.password.valid?
   end
 end
 
-describe 'Controller' do
-  describe "#index" do
-    let(:product_double) { double('Product double', all: []) }
-
-    before { stub_const 'Product', product_double }
-
-    it 'stubs the supplied method' do
-      controller = Controller.new
-
-      expect(controller.index).to eq []
+class ProductsController
+  def index
+    authenticated = UserAuthenticator.new(current_user).authenticate
+    if authenticated
+      Product.all
+    else
+      Product.none
     end
   end
+end
+
+describe ProductsController do
+  context 'when the user has valid credentials' do
+    before do
+      allow(UserAuthenticator).to receive(:authenticate).and_return(true)
+    end
+
+    it 'returns all products'
+  end
+
+  context 'when the user has valid credentials' do
+    before do
+      allow(UserAuthenticator).to receive(:authenticate).and_return(false)
+    end
+
+    it 'returns no products'
+  end
+
 end
