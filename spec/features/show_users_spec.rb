e2968e3632153f9460193c03390ef36c25f149ad
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-08 10:08:57 -0200

    Test 'back' link in show page.

--- spec/features/show_users_spec.rb
+++ spec/features/show_users_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
 describe 'As a user', js: true, type: :feature do
-  before do
-    User.create(name: 'User 1', email: 'user@email.com')
-  end
 
-  it 'I can view the details of other users' do
+  specify 'I can view the details of other users' do
+    user = User.create(name: 'User 1', email: 'user@email.com')
     visit '/users'
     expect(page).to have_content 'Users'
-    user = User.first
 
     find(".js-show-#{user.id}").click
 
     expect(page).to have_content 'User 1'
     expect(page).to have_content 'user@email.com'
+  end
+
+  context 'inside the user details page' do
+    let(:user) { User.create(name: 'User 1', email: 'user@email.com') }
+
+    before { visit "/users/#{user.id}" }
+
+    specify 'I can go back to the users list' do
+      click_link 'Back'
+
+      expect(page.current_path).to eq(users_path)
+    end
   end
 end
