bb10fc7005abf47c68da5f57a23e28d852d59a35
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-08 10:45:40 -0200

    Add creation, update and destruction feature specs

---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'As a user', js: true, type: :feature do
+
+  it 'I can create other users' do
+    visit '/users'
+
+    sleep 1
+
+    click_link 'New User'
+    sleep 1
+    expect(page.current_path).to eq(new_user_path)
+
+    fill_in 'Name', with: 'User 1'
+    sleep 1
+    check('Admin')
+    sleep 1
+    fill_in 'Email', with: 'user@email.com'
+    sleep 1
+    click_button 'Create User'
+
+    # Creation redirects to list
+    expect(page.current_path).to eq(users_path)
+
+    sleep 1
+    # Check flash message
+    expect(page).to have_content 'User was successfully created.'
+
+    first_tr = find('table tbody tr')
+
+    within(first_tr) do
+      expect(page).to have_content 'User 1'
+      expect(page).to have_content 'user@email.com'
+    end
+  end
+end
