2e95a66366c0e0b7b9e32328fdd51467df452c1f
Emiliano Leite <emiliano.leite@codus.com.br>
2017-12-12 14:25:11 -0200

    Delete unnused examples.

--- spec/mailers/previews/sample_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/sample_mailer
-class SampleMailerPreview < ActionMailer::Preview
-  def welcome_email
-    SampleMailer.welcome_email(User.first)
-  end
-end
